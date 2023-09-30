dfaf88cefb11ef660e2bd7e4589719dc98485bfa
Newstar12321 <tazeev.zhenya@mail.ru>
2023-09-30 20:03:24 +0700

    Добавьте файлы проекта.

---
+++ lab2_2/lab2_2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int num;
+    char f, s, l;
+    cout << "Введите трехзначное число: ";
+    cin >> num;
+    f = num / 100;
+    s = num % 100 / 10;
+    l = num % 10;
+    num = s * 100 + f * 10 + l;
+    cout << "Число, полученное при перестановке: " << num << endl;
+}
